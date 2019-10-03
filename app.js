c4366e12aaffdb5bf6771e9629f1773e887f3877
Inga-Z <z_inga@mail.ru>
2019-10-03 22:00:56 +0300

    1 итог

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log(worked);
+console.log('worked');
+
+const regularAmount = 10000;
+const increasCashbackAmount = 15000;
+const specialAmount = 20000;
+
+const regularPercent = 0.01;
+const increasCashbackPercent = 0.05;
+const specialPercent = 0.3;
+
+const cashback = regularAmount * regularPercent + increasCashbackAmount * increasCashbackPercent + specialAmount * specialPercent;
+
+console.log(cashback);
+
+const acceptblCashback = 3000;
+
+
